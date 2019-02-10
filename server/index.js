a5dcadd75f7a25c28e378f9fd83cecd27fe20d0e
Claudio Ferreira <claudio.zer0@gmail.com>
2019-02-10 13:00:28 -0200

    config
    Configurando servidor para caso não encontre uma porta rodar na porta 3000 dentro de settings
    
    em Starting, estamos iniciando o servidor

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+
+// Middlewares
+
+// Routes
+
+// Startting the server
+app.listen( app.get('port'), () => {
+    console.log('Server on port 3000');
+});
