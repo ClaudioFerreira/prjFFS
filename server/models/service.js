db332623af48447cc75b5c28f34745c74a1db2f1
Claudio Ferreira <claudio.zero0@gmail.com>
2019-02-12 11:24:49 -0200

    Imagem
    Provavel metodo para guardar a imagem usando o "Type: Buffer"
    Vou criar a interface primeiro para poder verificar

--- server/models/service.js
+++ server/models/service.js
@@ -3,8 +3,8 @@ const { Schema } = mongoose;
 
 const ServiceSchema = new Schema({
     title: { type: String, required: true },
-    description: { type: String, required: true }
-    // image: { type: } AINDA TENHO QUE DESCOBRIR COMO SUBIR UMA ARQUIVO DE IMAGEM
+    description: { type: String, required: true },
+    // image: { type: Buffer, required: true } //AINDA TENHO QUE DESCOBRIR COMO SUBIR UMA ARQUIVO DE IMAGEM
 });
 
 module.exports = mongoose.model('Service', ServiceSchema);
