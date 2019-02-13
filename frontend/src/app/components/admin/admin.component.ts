381cbbdd9d119c6c9a849c781f035de8ce583032
Claudio Ferreira <claudio.zero0@gmail.com>
2019-02-13 16:57:56 -0200

    clean form & add fom
    creado função que limpa o formulario
    
    criada a função que adiciona um novo documento na coleção de serviços

--- frontend/src/app/components/admin/admin.component.ts
+++ frontend/src/app/components/admin/admin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms'
 
 import { ServiceService } from '../../services/service.service';
+import { Service } from 'src/app/models/service';
 
 @Component({
   selector: 'app-admin',
@@ -13,6 +15,21 @@ export class AdminComponent implements OnInit {
   constructor(private serviceService: ServiceService) { }
 
   ngOnInit() {
+  }
+
+  addService(form: NgForm){
+    this.serviceService.postService(form.value)
+    .subscribe(res => {
+      this.resetForm(form);
+      console.log('Service Saved');
+    })
+  }
+
+  resetForm(form?: NgForm){
+    if (form) {
+      form.reset();
+      this.serviceService.selectedService = new Service();
+    }
   }
 
 }
