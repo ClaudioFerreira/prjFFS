46a2567ddad674acc0bfd1103bfbaab9beeaf328
Claudio Ferreira <claudio.zero0@gmail.com>
2019-02-13 08:28:03 -0200

    admin & models & services
    criado o component admin onde teremo s o painel administrativo
    
    criado o diretorio models
    
    criado o diretorio services

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
