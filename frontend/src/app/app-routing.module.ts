40c159cfdc65b14ed91c1c9be3c6c007208aa086
Claudio <claudio.zero0@gmail.com>
2019-02-25 21:08:59 -0300

    Rotas
    Criando as rotas dos componentes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule, Router } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+import { AdminComponent } from './components/admin/admin.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      { path: '', component: HomeComponent }
+    ]
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+  }
+  
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
