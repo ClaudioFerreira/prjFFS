23c00cc696f2fd445f43c768550b4e6e10d5208b
Claudio Ferreira <claudio.zero0@gmail.com>
2019-02-27 17:00:10 -0300

    Navbar social media
    adde menu de media social
    Chamando services no home

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {} from '../../services/service.service'
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
